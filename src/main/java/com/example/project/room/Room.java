2e821ed0be18215bdaa77d280ac6b790cb6cf9c0
ssong-hg <110438382+ssong-hg@users.noreply.github.com>
2022-12-20 14:33:02 +0900

    Update Room.java
    최초 실행시 기본 data 저장

--- src/main/java/com/example/project/room/Room.java
+++ src/main/java/com/example/project/room/Room.java
@@ -5,16 +5,19 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Room {
 	@Id
 	@NotNull
-
 	private String  roomNo;
 	@Column(length = 200)
 	private String roomType;
